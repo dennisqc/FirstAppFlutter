b82422d5b176fc624a834df47cb3d210f2122cca
unknown <dennisquispe72>
2024-06-08 22:28:09 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,59 @@ void main() {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Widget textT() {
+    return Text(
+      "T",
+      style: TextStyle(fontSize: 150, color: Colors.white),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 43, 131, 203),
+          backgroundColor: const Color(0xffA79277),
           title: Text(
             "Mi primera chamba",
             // style: TextStyle(color: Colors.yellow),
           ),
+          centerTitle: true,
         ),
         body: Container(
           padding: EdgeInsets.all(20),
           color: Colors.yellow,
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Container(
-              color: Colors.green,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "T",
-                    style: TextStyle(fontSize: 150),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  height: 300,
+                  color: Colors.green,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      textT(),
+                      /* SizedBox(
+                    width: 24,
+                  ),*/
+                      Container(
+                        width: 20,
+                        color: Colors.blue,
+                        height: 150,
+                      ),
+                      Icon(
+                        Icons.arrow_downward_rounded,
+                        color: Colors.yellow,
+                        size: 150,
+                      )
+                    ],
                   ),
-                  Container(
-                    width: 20,
-                    color: Colors.blue,
-                    height: 150,
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: Colors.yellow,
-                    size: 150,
-                  )
-                ],
-              ),
-            ),
-          ]),
+                ),
+                textT(),
+                textT(),
+              ]),
         ),
       ),
     );
