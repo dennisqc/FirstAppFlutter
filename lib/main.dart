a78fba315704723a8e1541509269645199d49451
unknown <dennisquispe72>
2024-06-09 20:01:52 -0500

    plantilla inicial

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,115 @@ void main() {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  Widget textT() {
-    return Text(
-      "T",
-      style: TextStyle(fontSize: 150, color: Colors.white),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          leading: Icon(Icons.abc),
-          backgroundColor: const Color(0xffA79277),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
           title: Text(
             "Mi primera chamba",
             // style: TextStyle(color: Colors.yellow),
           ),
           centerTitle: true,
         ),
-        body: Container(
-          padding: EdgeInsets.all(20),
-          color: Colors.yellow,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: 300,
-                  width: 300,
-                  color: Colors.green,
-                  child: Image.network("https://static.vecteezy.com/system/resources/thumbnails/030/028/888/small_2x/crazy-spider-man-furious-mad-portrait-expressive-illustration-artwork-oil-painted-sketch-tattoo-photo.jpg")
-                ),
-                Image.asset("assets/images/spiderman.jpg",
-                fit: BoxFit.contain,)
-                //textT(),
-                //textT(),
-              ]),
+        body: Column(
+          children: [
+            Image.network(
+                "https://viajes.nationalgeographic.com.es/medio/2018/03/01/machu-picchu_5ff969ae_1280x720.jpg"),
+            Padding(
+              padding: const EdgeInsets.all(38.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Machu Picchu",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Peru",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.orange,
+                      ),
+                      Text("41")
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 32, right: 32, bottom: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.call,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text("Call me")
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.near_me,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text("Route")
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.share,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text("Shaer")
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                padding: const EdgeInsets.only(right: 28, left: 28),
+                child: Text(
+                    "Lorem ipsum is a placeholder text used in graphic design and publishing to demonstrate the visual style of a document, webpage, or typeface. It's intended to be a sample, not to be read as meaningful sentences. Lorem ipsum is essentially nonsense language, a garbled fragment of a longer Latin text written by the Roman philosopher Cicero in the 1st century BCE."))
+          ],
         ),
       ),
     );
