3950c38f3f9017bea1cc2a991ac8fa2c92ae5644
unknown <dennisquispe72>
2024-06-12 19:59:47 -0500

    ultimo

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const titulo = TextStyle(fontSize: 60);
--- lib/main.dart
+++ lib/main.dart
@@ -1,123 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:myfirstproject/constants.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: HomePage(),
-  ));
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // const HomePage({super.key});
+  int x = 0;
+  int y = 0;
+  void increment() {
+    x += 100;
+    y += 100;
+    setState(() {});
+  }
+
+  int increment100(int variable) {
+    return variable += 100;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () {},
-              );
-            },
-          ),
-          title: Text(
-            "Bienvenido",
-            // style: TextStyle(color: Colors.yellow),
-          ),
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-              tooltip: "Buscar",
-            ),
-            IconButton(
-              icon: Icon(Icons.save_alt),
-              onPressed: () {},
-              tooltip: "Descargar",
-            ),
-          ],
-        ),
-        body: Column(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 33, 39, 213),
+        title: Text("Stateles and satefull"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-                "https://viajes.nationalgeographic.com.es/medio/2018/03/01/machu-picchu_5ff969ae_1280x720.jpg"),
-            Container(
-                padding: const EdgeInsets.all(18),
-                child: Text(
-                  "Monarch population soars 4,900 percent since last in thrilling 2021 western migration",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'RobotoMono'),
-                )),
-            Padding(
-              padding: const EdgeInsets.only(right: 18, left: 18),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Image.network(
-                        "https://www.dzoom.org.es/wp-content/uploads/2018/02/fondos-retratos-16-1024x682.jpg",
-                        fit: BoxFit.contain,
-                        height: 50,
-                        width: 50,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Text("By Andy Con"),
-                    ],
-                  ),
-                  Column(
-                    children: [Text("1Hr ago")],
-                  )
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "x",
+                      style: titulo,
+                    ),
+                    Text(
+                      x.toString(),
+                      style: titulo,
+                    )
+                  ],
+                ),
+                VerticalDivider(
+                  color: Colors.black,
+                  width: 100,
+                  thickness: 2,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      "y",
+                      style: titulo,
+                    ),
+                    Text(
+                      y.toString(),
+                      style: titulo,
+                    )
+                  ],
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  barIcon(("8 Coments"), Icons.textsms_outlined),
-                  barIcon("34 Likes", Icons.favorite_border),
-                  barIcon("Share", Icons.share_outlined)
-                ],
-              ),
+            Divider(
+              thickness: 2,
             ),
-            Container(
-                padding: const EdgeInsets.only(right: 18, left: 18),
-                child: Text(
-                    "Lorem ipsum is a placeholder text used in graphic design and publishing to demonstrate the visual style of a document, webpage, or typeface. It's intended to be a sample, not to be read as meaningful sentences. Lorem ipsum is essentially nonsense language, a garbled fragment of a longer Latin text written by the Roman philosopher Cicero in the 1st century BCE.")),
-            Container(
-                padding: const EdgeInsets.only(right: 18, left: 18),
-                child: Text(
-                    "Lorem ipsum is a placeholder text used in graphic design and publishing to demonstrate the visual style of a document, webpage, or typeface. It's intended to be a sample, not to be read as meaningful sentences. Lorem ipsum is essentially nonsense language, a garbled fragment of a longer Latin text written by the Roman philosopher Cicero in the 1st century BCE.")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      x = increment100(x);
+                      setState(() {});
+                      print("Hola PResionaste");
+                    },
+                    child: Text("Cambiar x ")),
+                ElevatedButton(
+                    onPressed: () {
+                      y = increment100(y);
+                      setState(() {});
+                      print("Hola PResionaste");
+                    },
+                    child: Text("Cambiar y "))
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  increment();
+                  print("Hola PResionaste");
+                },
+                child: Text("Cambiar a ambos "))
           ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.arrow_upward_outlined),
         ),
       ),
     );
   }
-}
-
-Widget barIcon(String accion, IconData icono) {
-  return Row(
-    children: [
-      Icon(
-        icono,
-      ),
-      SizedBox(
-        width: 8,
-      ),
-      Text(accion)
-    ],
-  );
 }
