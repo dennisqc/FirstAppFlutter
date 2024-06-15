1357ebbba716ed93e2e9e7823f8911c620ab85cc
unknown <dennisquispe72>
2024-06-14 21:05:59 -0500

    dados

--- lib/constants.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/material.dart';
-
-const titulo = TextStyle(fontSize: 60);
--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:myfirstproject/constants.dart';
+import 'package:myfirstproject/pages/dice_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: DicePage(),
+      debugShowCheckedModeBanner: false,
     ),
   );
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  // const HomePage({super.key});
-  int x = 0;
-  int y = 0;
-  void increment() {
-    x += 100;
-    y += 100;
-    setState(() {});
-  }
-
-  int increment100(int variable) {
-    return variable += 100;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 33, 39, 213),
-        title: Text("Stateles and satefull"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      "x",
-                      style: titulo,
-                    ),
-                    Text(
-                      x.toString(),
-                      style: titulo,
-                    )
-                  ],
-                ),
-                VerticalDivider(
-                  color: Colors.black,
-                  width: 100,
-                  thickness: 2,
-                ),
-                Column(
-                  children: [
-                    Text(
-                      "y",
-                      style: titulo,
-                    ),
-                    Text(
-                      y.toString(),
-                      style: titulo,
-                    )
-                  ],
-                )
-              ],
-            ),
-            Divider(
-              thickness: 2,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      x = increment100(x);
-                      setState(() {});
-                      print("Hola PResionaste");
-                    },
-                    child: Text("Cambiar x ")),
-                ElevatedButton(
-                    onPressed: () {
-                      y = increment100(y);
-                      setState(() {});
-                      print("Hola PResionaste");
-                    },
-                    child: Text("Cambiar y "))
-              ],
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  increment();
-                  print("Hola PResionaste");
-                },
-                child: Text("Cambiar a ambos "))
-          ],
-        ),
-      ),
-    );
-  }
 }
